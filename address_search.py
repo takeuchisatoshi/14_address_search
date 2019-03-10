b7217997650e45c1fbc9f69099c0f55c79193910
takeuchisatoshi <satsitt3@gmail.com>
2019-03-10 17:59:06 +0900

    main関数を追加

--- address_search.py
+++ address_search.py
@@ -1,16 +1,23 @@
 import requests
 
-if __name__ == '__main__':
+
+def main():
     input_data = input("郵便番号(7ケタ)?: ")
+
     zipcode = input_data
 
     url = f"http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
-    r = requests.get(url)
-    address_dict = r.json()
-    # print(r.json())
 
+    r = requests.get(url)
+
+    address_dict = r.json()
+
+    # print(r.json())
     address1 = address_dict["results"][0]["address1"]
     address2 = address_dict["results"][0]["address2"]
     address3 = address_dict["results"][0]["address3"]
-
     print(f"{address1}{address2}{address3}")
+
+
+if __name__ == '__main__':
+    main()
