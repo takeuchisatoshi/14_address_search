512d96466650bb34ac67d13be25af3f5b79c2326
takeuchisatoshi <satsitt3@gmail.com>
2019-03-10 17:41:46 +0900

    address_dictから地名を出力

--- address_search.py
+++ address_search.py
@@ -2,7 +2,12 @@ import requests
 
 if __name__ == '__main__':
     url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=0287111"
-
     r = requests.get(url)
+    address_dict = r.json()
+    # print(r.json())
 
-    print(r.json())
+    address1 = address_dict["results"][0]["address1"]
+    address2 = address_dict["results"][0]["address2"]
+    address3 = address_dict["results"][0]["address3"]
+
+    print(f"{address1}{address2}{address3}")
